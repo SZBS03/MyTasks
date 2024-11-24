d9150be60664be80ad49ad3c6bc7713ddd9842f8
SZBS03 <zaidbinshams03@gmail.com>
2024-11-24 02:13:54 -0800

    updated

--- Aiml/lab8 (1).py
+++ Aiml/lab8 (1).py
@@ -1,129 +1,85 @@
 import numpy as np
+import random
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation, PillowWriter
+from matplotlib import animation
 
-# Define the Rosenbrock function
-def objective_function(x, y):
-    return (1 - x)**2 + 100 * (y - x**2)**2
+def fitness_function(x1, x2):
+    f1 = x1 + 2 * -x2 + 3
+    f2 = 2 * x1 + x2 - 8
+    z = f1**2 + f2**2
+    return z
 
-class ParticleSwarmOptimizer:
-    def __init__(self):
-        # PSO parameters
-        self.num_particles = 30
-        self.max_iter = 100
-        self.w = 0.5      # inertia weight
-        self.c1 = 1.5     # cognitive weight
-        self.c2 = 1.5     # social weight
-        
-        # Bounds for the search space
-        self.x_min, self.x_max = -5, 5
-        self.y_min, self.y_max = -5, 5
-        
-        # Initialize particle positions and velocities
-        self.particles_pos = np.random.uniform(
-            [self.x_min, self.y_min], 
-            [self.x_max, self.y_max], 
-            (self.num_particles, 2)
-        )
-        self.particles_vel = np.random.uniform(-1, 1, (self.num_particles, 2))
-        
-        # Initialize personal and global bests
-        self.personal_best_pos = self.particles_pos.copy()
-        self.personal_best_val = np.array([
-            objective_function(x, y) for x, y in self.particles_pos
-        ])
-        self.global_best_pos = self.personal_best_pos[
-            np.argmin(self.personal_best_val)
-        ]
-        self.global_best_val = np.min(self.personal_best_val)
-        
-        # Setup visualization
-        self.setup_visualization()
-        
-    def setup_visualization(self):
-        self.fig = plt.figure(figsize=(10, 8))
-        self.ax = self.fig.add_subplot(111)
-        
-        # Create mesh for contour plot
-        x = np.linspace(self.x_min, self.x_max, 100)
-        y = np.linspace(self.y_min, self.y_max, 100)
-        X, Y = np.meshgrid(x, y)
-        Z = objective_function(X, Y)
-        
-        # Plot the objective function surface and store the mappable
-        contour = self.ax.contourf(X, Y, Z, 50, cmap='viridis', alpha=0.6)
-        self.scatter = self.ax.scatter(
-            self.particles_pos[:, 0], 
-            self.particles_pos[:, 1], 
-            c='red', 
-            s=50,
-            alpha=0.8
-        )
-        
-        # Add colorbar using the stored contour mappable
-        plt.colorbar(contour, label='Objective Function Value')
-        self.ax.set_xlabel('X')
-        self.ax.set_ylabel('Y')
-        
-    def update(self, frame):
-        # Update velocities and positions
-        r1, r2 = np.random.rand(2)
-        
-        # Vectorized velocity update
-        self.particles_vel = (
-            self.w * self.particles_vel +
-            self.c1 * r1 * (self.personal_best_pos - self.particles_pos) +
-            self.c2 * r2 * (self.global_best_pos - self.particles_pos)
-        )
-        
-        # Update positions
-        self.particles_pos += self.particles_vel
-        
-        # Ensure particles stay within bounds
-        self.particles_pos = np.clip(
-            self.particles_pos,
-            [self.x_min, self.y_min],
-            [self.x_max, self.y_max]
-        )
-        
-        # Evaluate objective function for all particles
-        current_vals = np.array([
-            objective_function(x, y) for x, y in self.particles_pos
-        ])
-        
-        # Update personal bests
-        mask = current_vals < self.personal_best_val
-        self.personal_best_val[mask] = current_vals[mask]
-        self.personal_best_pos[mask] = self.particles_pos[mask]
-        
-        # Update global best
-        min_val_idx = np.argmin(self.personal_best_val)
-        self.global_best_pos = self.personal_best_pos[min_val_idx]
-        self.global_best_val = self.personal_best_val[min_val_idx]
-        
-        # Update visualization
-        self.scatter.set_offsets(self.particles_pos)
-        self.ax.set_title(
-            f'Iteration {frame+1}, Global Best: {self.global_best_val:.3f}'
-        )
-        return self.scatter,
+def update_velocity(particle, velocity, pbest, gbest, w_min=0.5, max=1.0, c=0.1):
+    num_particle = len(particle)
+    new_velocity = np.array([0.0 for i in range(num_particle)])
+    r1 = random.uniform(0, max)
+    r2 = random.uniform(0, max)
+    w = random.uniform(w_min, max)
+    c1 = c
+    c2 = c
+    for i in range(num_particle):
+        new_velocity[i] = w*velocity[i] + c1*r1*(pbest[i]-particle[i])+c2*r2*(gbest[i]-particle[i])
+    return new_velocity
+
+def update_position(particle, velocity):
+    new_particle = particle + velocity
+    return new_particle
+
+def pso_2d(population, dimension, position_min, position_max, generation, fitness_criterion):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
     
-    def run(self):
-        ani = FuncAnimation(
-            self.fig,
-            self.update,
-            frames=self.max_iter,
-            interval=50,
-            blit=True
-        )
-        # Save the animation as a GIF
-        ani.save("pso_animation.gif", writer=PillowWriter(fps=20))
-        print("Animation saved as 'pso_animation.gif'")
+    x = np.linspace(position_min, position_max, 80)
+    y = np.linspace(position_min, position_max, 80)
+    X, Y = np.meshgrid(x, y)
+    Z = fitness_function(X, Y)
+    ax.plot_wireframe(X, Y, Z, color='r', linewidth=0.2)
+    
+    images = []
+    particles = [[random.uniform(position_min, position_max) for j in range(dimension)] for i in range(population)]
+    pbest_position = particles
+    pbest_fitness = [fitness_function(p[0], p[1]) for p in particles]
+    gbest_index = np.argmin(pbest_fitness)
+    gbest_position = pbest_position[gbest_index]
+    velocity = [[0.0 for j in range(dimension)] for i in range(population)]
+    
+    for t in range(generation):
+        if np.average(pbest_fitness) <= fitness_criterion:
+            break
+        else:
+            for n in range(population):
+                velocity[n] = update_velocity(particles[n], velocity[n], pbest_position[n], gbest_position)
+                particles[n] = update_position(particles[n], velocity[n])
+            pbest_fitness = [fitness_function(p[0], p[1]) for p in particles]
+            gbest_index = np.argmin(pbest_fitness)
+            gbest_position = pbest_position[gbest_index]
+            image = ax.scatter3D(
+                [particles[n][0] for n in range(population)],
+                [particles[n][1] for n in range(population)],
+                [fitness_function(particles[n][0], particles[n][1]) for n in range(population)],
+                c='b'
+            )
+            images.append([image])
+    
+    print('Global Best Position: ', gbest_position)
+    print('Best Fitness Value: ', min(pbest_fitness))
+    print('Average Particle Best Fitness Value: ', np.average(pbest_fitness))
+    print('Number of Generation: ', t)
+    
+    animated_image = animation.ArtistAnimation(fig, images)
+    animated_image.save('./pso_simple.gif', writer='pillow')
+    
+    plt.show()
+    return gbest_position, min(pbest_fitness)
 
-def main():
-    optimizer = ParticleSwarmOptimizer()
-    optimizer.run()
+population = 100
+dimension = 2
+position_min = -100.0
+position_max = 100.0
+generation = 400
+fitness_criterion = 10e-4
 
-if __name__ == "__main__":
-    main()
-
+best_position, best_fitness = pso_2d(population, dimension, position_min, position_max, generation, fitness_criterion)
